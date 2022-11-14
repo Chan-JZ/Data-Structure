ccc7bda281e8d8d9a9054206057497dc732957af
Chan-JZ <chanjianzhe2002@gmail.com>
2022-11-14 23:32:05 +1030

    updates

---
+++ LinkedList.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+
+class LinkedList
+{
+    Node *head;
+
+public:
+    LinkedList(int x) {
+        this->head = new Node();
+        this->head->data = x;
+        this->head->next = NULL;
+    }
+
+    Node *getHead() {
+        return this->head;
+    }
+
+    void addFirst(int x) {
+        Node *temp = new Node();
+        temp->data = x;
+        temp->next = this->head;
+        this->head = temp;
+
+        return;
+    }
+
+    void addLast(int x) {
+        Node *temp = new Node();
+        temp->data = x;
+        temp->next = NULL;
+
+        Node *ptr = this->head;
+        while (ptr->next != NULL) ptr = ptr->next;
+        
+        ptr->next = temp;
+
+        return;
+    }
+
+    void deleteFirst() {
+        Node *temp = this->head;
+        this->head = this->head->next;
+        delete temp;
+
+        return;
+    }
+
+    void deleteLast() {
+        Node *ptr = this->head;
+        while (ptr->next->next != NULL) ptr = ptr->next;
+
+        delete ptr->next;
+        ptr->next = NULL;
+
+        return;
+    }
+
+    void printList() {
+        Node *temp = this->head;
+
+        while(temp != NULL) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+
+        std::cout << std::endl;
+
+        return;
+    }
+};
+
+int main() {
+    LinkedList *l1 = new LinkedList(10);
+    l1->addFirst(11);
+    l1->addFirst(12);
+    l1->addFirst(13);
+    l1->printList();
+    l1->addLast(20);
+    l1->addLast(21);
+    l1->addLast(22);
+    l1->printList();
+    l1->deleteFirst();
+    l1->printList();
+    l1->deleteLast();
+    l1->printList();
+}
---
+++ LinkedList2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+
+void insertHead(Node **head, int x) {
+    Node *temp = new Node();
+    temp->data = x;
+    temp->next = *head;
+
+    *head = temp;
+}
+
+void insertTail(Node *head, int x) {
+    while (head->next != NULL) {
+        head = head->next;
+    }
+
+    Node *temp = new Node();
+    temp->data = x;
+    temp->next = NULL;
+
+    head->next = temp;
+}
+
+void printList(Node *head) {
+    while (head != NULL) {
+        std::cout << head->data << std::endl;
+        head = head->next;
+    }
+}
+
+int main() {
+    Node *head;
+    head->data = 1;
+    head->next = NULL;
+
+    insertHead(&head, 10);
+    insertHead(&head, 11);
+
+
+    insertTail(head, 100);
+    insertTail(head, 101);
+    printList(head);
+
+}
